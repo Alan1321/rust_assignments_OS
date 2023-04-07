6b69f8a1890e512e2c227b7de922d432532b2989
Alan123 <alansubedi123@gmail.com>
2023-04-07 01:12:47 -0500

    It Works, needs modifications tho

--- program3/passignment3/src/main.rs
+++ program3/passignment3/src/main.rs
@@ -6,7 +6,6 @@ Used: VS Code on Ubuntu/WSL.
 */
 
 use std::io;
-use std::collections::VecDeque;
 use std::collections::BinaryHeap;
 use std::cmp::Ordering;
 use rand::Rng;
@@ -15,10 +14,11 @@ use std::time::Duration;
 use std::sync::{Arc, Mutex};
 
 //the process structure for our nodes
+#[derive(Clone)]
 struct __Process {
-    id: i32,
-    priority: u32,
-    sleep_time: u32,
+    id: u64,
+    priority: u64,
+    sleep_time: u64,
     description: String
 }
 
@@ -46,128 +46,105 @@ impl PartialEq for __Process {
 //adding eq as well, without this it won't work
 impl Eq for __Process {}
 
-fn main(){
+fn main() {
+    // Prompt the user for input
+    let n = accept_input("Enter number of processes to generate each time: ".to_string());
+    let s = accept_input("Enter sleep time in ms between generations: ".to_string());
+    let m = accept_input("Enter number of times the producer should generate processes: ".to_string());
 
-    //accept n -- also check n errors
-    let mut _x: i32 = 0;
+    // Create a shared binary heap that will serve as the priority queue
+    let heap = Arc::new(Mutex::new(BinaryHeap::new()));
+
+    // Spawn the producer thread
+    let producer_heap = Arc::clone(&heap);
+    let producer_thread = thread::spawn(move || {
+        for _ in 0..m {
+            // Generate n processes with random priorities and add them to the heap
+            for i in 0..n {
+                let priority: u64 = rand::thread_rng().gen_range(0..100);
+                let sleep_time: u64 = rand::thread_rng().gen_range(100..2000);
+                producer_heap.lock().unwrap().push(__Process{id:i, priority:priority, sleep_time:sleep_time, description:format!("{}{}", "Process Node: ", i+1)});
+            }
+            // Sleep for s milliseconds
+            thread::sleep(Duration::from_millis(s));
+        }
+    });
+
+    // Wait for a short delay to ensure there are nodes in the heap before consumers start
+    thread::sleep(Duration::from_millis(100));
+
+    // Spawn the consumer threads
+    let consumer_heap = Arc::clone(&heap);
+    let consumer_thread1 = thread::spawn(move || {
+        loop {
+            let process_opt = consumer_heap.lock().unwrap().pop();
+            match process_opt {
+                Some(process) => {
+                    println!(
+                        "Consumer 1: Executing process {} with priority {} for 100ms",
+                        process.id, process.priority
+                    );
+                    thread::sleep(Duration::from_millis(1000));
+                }
+                None => {
+                    println!("Consumer 1: No more processes to execute");
+                    break;
+                }
+            }
+        }
+    });
+
+    let consumer_heap = Arc::clone(&heap);
+    let consumer_thread2 = thread::spawn(move || {
+        loop {
+            let process_opt = consumer_heap.lock().unwrap().pop();
+            match process_opt {
+                Some(process) => {
+                    println!(
+                        "Consumer 2: Executing process {} with priority {} for 100ms",
+                        process.id, process.priority
+                    );
+                    thread::sleep(Duration::from_millis(1000));
+                }
+                None => {
+                    println!("Consumer 2: No more processes to execute");
+                    break;
+                }
+            }
+        }
+    });
+
+    // Wait for all threads to complete
+    producer_thread.join().unwrap();
+    consumer_thread1.join().unwrap();
+    consumer_thread2.join().unwrap();
+}
+
+
+fn accept_input(msg: String) -> u64{
+    //accept input -- also check input errors
+    let mut _x: u64 = 0;
     loop {
-        //These 3 lines accepts user n as String
-        println!("Enter N (the number of process nodes to generate each time): ");
-        let mut n = String::new();
-        io::stdin().read_line(&mut n).expect("failed to read line");
+        //These 3 lines accepts user input as String
+        println!("{}", msg);
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("failed to read line");
 
         //Here we convert String to int and also do error handling for strings
-        let _num: i32 = match n.trim().parse() {
+        let _num: u64 = match input.trim().parse() {
             Ok(_num) => {
                 if _num < 0{
-                    println!("Invalid n :( .Needs to be greater than 0.");
+                    println!("Invalid Input :( .Needs to be greater than 0.");
                     continue;
                 }
                 _x = _num;
                 break;
             },
             Err(_) => {
-                println!("Invalid n :(\n");
+                println!("Invalid Input :(\n");
                 continue;
             }
         };
     }
-
-    //accept s (sleep time in ms between generations) -- also check errors
-    let mut _y: i32 = 0;
-    loop {
-        println!("Enter S (the sleep time in ms between generations): ");
-        let mut s = String::new();
-        io::stdin().read_line(&mut s).expect("failed to read line");
-
-        //Here we convert String to int and also do error handling for strings
-        let _num: i32 = match s.trim().parse() {
-            Ok(_num) => {
-                if _num < 0{
-                    println!("Invalid n :( .Needs to be greater than 0.");
-                    continue;
-                }
-                _y = _num;
-                break;
-            },
-            Err(_) => {
-                println!("Invalid n :(\n");
-                continue;
-            }
-        };
-    }
-
-
-    //accept s (sleep time in ms between generations) -- also check errors
-    let mut _z: i32 = 0;
-    loop {
-        println!("Enter M (number of times the producer should generate N nodes): ");
-        let mut s = String::new();
-        io::stdin().read_line(&mut s).expect("failed to read line");
-
-        //Here we convert String to int and also do error handling for strings
-        let _num: i32 = match s.trim().parse() {
-            Ok(_num) => {
-                if _num < 0{
-                    println!("Invalid n :( .Needs to be greater than 0.");
-                    continue;
-                }
-                _z = _num;
-                break;
-            },
-            Err(_) => {
-                println!("Invalid n :(\n");
-                continue;
-            }
-        };
-    }
-
-    println!("Now creating and adding {} process nodes to a Queue and to a binary minheap", _x);
-    //init vecdeque and heap
-    let mut buf_queue = VecDeque::new();
-    let mut buf_heap: BinaryHeap<__Process> = BinaryHeap::new();
-
-    //Make Process Nodes and Add to queue and heap
-    let mut i = 0;
-    while i < _x{
-
-        let priority: u32 = rand::thread_rng().gen_range(0..100);
-        let sleep_time: u32 = rand::thread_rng().gen_range(100..2000);
-
-        buf_queue.push_back(__Process{id:i, priority:priority, sleep_time:sleep_time, description:format!("{}{}", "Process Node: ", i+1)});
-        buf_heap.push(__Process{id:i, priority:priority, sleep_time:sleep_time, description:format!("{}{}", "Process Node: ", i+1)});
-
-        i = i + 1;
-    }
-
-    //Queue Size
-    let queue_size = buf_queue.len();
-    //Heap Size
-    let heap_size = buf_heap.len();
-
-    //Verify Size of Queue and Heap
-    println!("Verifying.  The queue contains {} elements", queue_size);
-    println!("Verifying.  The heap contains {} elements\n", heap_size);
-
-    //Pop from Queue and Print
-    println!("Now, draining the Queue, one process at a time ... ");
-    let mut j = 0;
-    while j < queue_size {
-        let node = buf_queue.pop_front().unwrap();
-        println!("Pid: {}, pri: {}, sleep: {}, desc: {}", node.id, node.priority, node.sleep_time, node.description);
-        j = j+1;
-    }
-
-    println!("");
-
-    //Pop from heap and print
-    println!("Now, draining the MinHeap, one process at a time ...");
-    let mut k = 0;
-    while k < heap_size {
-        let node = buf_heap.pop().unwrap();
-        println!("Pid: {}, pri: {}, sleep: {}, desc: {}", node.id, node.priority, node.sleep_time, node.description);
-        k = k+1;
-    }
-
-    println!("\nGoodbye.");
+    return _x
 }
